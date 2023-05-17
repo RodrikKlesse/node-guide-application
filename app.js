15dd3ad11af684b53e1d1575c8e4de31a4134a5f
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-05-17 11:47:29 -0300

    Node course - add express to the project

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello form Express!</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
