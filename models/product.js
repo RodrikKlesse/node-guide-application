afc916db2b371702bce5f399e96f4dc90a845f5a
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-06-20 10:27:18 -0300

    Node course - save products on database

--- models/product.js
+++ models/product.js
@@ -12,18 +12,21 @@ module.exports = class Product {
     }
 
     save() {
-        
+        return db.execute(
+            'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
+            [this.title, this.price, this.description, this.imageUrl]
+        );
     }
 
     static deleteById(id) {
-        
+
     }
 
     static fetchAll() {
-       return db.execute('SELECT * FROM products');
+        return db.execute('SELECT * FROM products');
     }
 
     static findById(id) {
-       
+
     }
 };
