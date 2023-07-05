3e06a72935ff77bbe116141e90a514f8d4e3f302
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-07-05 14:32:48 -0300

    Node course - storing cart items as order items

--- controllers/shop.js
+++ controllers/shop.js
@@ -121,6 +121,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
+exports.postOrder = (req, res, next) => {
+    req.user
+        .getCart()
+        .then(cart => {
+            return cart.getProducts();
+        })
+        .then(products => {
+            return req.user
+                .createOrder()
+                .then(order => {
+                    order.addProducts(
+                        products.map(product => {
+                            product.orderItem = { quantity: product.cartItem.quantity };
+                            return product;
+                        })
+                    );
+                })
+                .catch(err => console.log(err));
+        })
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+};
+
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
