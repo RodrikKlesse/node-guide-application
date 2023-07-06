4059a76b44a4d2205c98d02cedbf862d8ebd2b06
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-07-06 14:49:22 -0300

    Node course - connecting database and creating a product

--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
     MongoClient.connect('mongodb+srv://rodrikhsk:vYicLJxCLSCaXPQW@cluster0.kxizuji.mongodb.net/?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected!');
-            callback(client);
+            _db = client.db();
+            callback();
         })
         .catch(err => {
-            console.log(err)
+            console.log(err);
+            throw err;
         });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No databse found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
