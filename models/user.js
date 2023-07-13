5afd278fc7494f38d9ca76b9c22a03fb96ba2af9
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-07-13 09:54:10 -0300

    Node course - adding the add to cart functionality

--- models/user.js
+++ models/user.js
@@ -4,14 +4,27 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, _id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id = _id;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        const updateCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updateCart } }
+            );
     }
 
     static findById(userId) {
