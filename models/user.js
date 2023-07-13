ed92a0e1c6b6ffa438f3bff54fc90b81be9c55ba
RodrikKlesse <rodrik_hsk@hotmail.com>
2023-07-13 16:11:34 -0300

    Node course - deleting cart items

--- models/user.js
+++ models/user.js
@@ -70,6 +70,19 @@ class User {
             });
     }
 
+    deleteItemFromCart(productId) {
+        const updateCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
+        });
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: { items: updateCartItems } } }
+            );
+    }
+
     static findById(userId) {
         const db = getDb();
         return db
