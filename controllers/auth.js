def3bfd0af76e68db98ac4297522ca8b2bd39386
RodrikKlesse <rodrik_hsk@hotmail.com>
2024-08-27 09:07:58 -0300

    Adding Login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,6 +50,16 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
@@ -82,6 +92,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
